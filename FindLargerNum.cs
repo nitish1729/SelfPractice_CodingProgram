195a6613e7c2fe9ec55b192076d9fabdcf93f868
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-14 03:43:32 +0530

    Greatest Number

--- FindLargerNum.cs
+++ FindLargerNum.cs
@@ -1,21 +1,21 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SelfPractice_CodingProgramCSharp
-//{
-//    public class FindLargerNum
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Console.WriteLine("Enter Num1");
-//            int num1 = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Enter Num2");
-//            int num2 = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Enter Num3");
-//            int num3 = int.Parse(Console.ReadLine());
+namespace SelfPractice_CodingProgramCSharp
+{
+    public class FindLargerNum
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Num1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Num2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Num3");
+            int num3 = int.Parse(Console.ReadLine());
             //if (num1 > num2)
             //{
             //    Console.WriteLine("Number1 is greater");
@@ -36,32 +36,34 @@
             //{
             //    Console.WriteLine("Number3 is greater");
             //}
-//            if (num1 > num2)
-//            {
-//                // nested if condition
-//                if (num1 > num3)
-//                {
-//                    Console.WriteLine("Number1 is greater");
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Number3 is greater");
+            //Console.WriteLine(".............................");
 
-//                }
-//            }
-//            else
-//            {
-                
-//                if (num2 > num3)
-//                {
-//                    Console.WriteLine("Number2 is greater");
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Number3 is greater");
-//                }
-//            }
-//            Console.ReadLine();
-//        }
-//    }
-//}
+            if (num1 > num2)
+            {
+                // nested if condition
+                if (num1 > num3)
+                {
+                    Console.WriteLine("Number1 is greater");
+                }
+                else
+                {
+                    Console.WriteLine("Number3 is greater");
+
+                }
+            }
+            else
+            {
+
+                if (num2 > num3)
+                {
+                    Console.WriteLine("Number2 is greater");
+                }
+                else
+                {
+                    Console.WriteLine("Number3 is greater");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
