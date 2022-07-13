6924e2322614d4f1316e7ad7deb0c2b34bea0eeb
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-14 03:37:30 +0530

    EvenOrOdd

---
+++ One_EveonOrOdd.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfPractice_CodingProgramCSharp
+{
+    public class One_EveonOrOdd
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the firstnumber");
+            int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the SecondNumber");
+            //int num2 = int.Parse(Console.ReadLine());
+            //int result = num1 % num2;
+            //if (result == 0)
+            //{
+            //    Console.WriteLine("Entered number is even");
+            //}
+            //else
+            //{
+
+            //   Console.WriteLine("Entered number is Odd");
+
+            //}
+            // Console.WriteLine(".....................................");
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Entered number is even");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
