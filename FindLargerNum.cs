5ceb9549ba9153748e358c68906b7ff179a23d44
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-17 02:08:46 +0530

    Find greater Number

--- FindLargerNum.cs
+++ FindLargerNum.cs
@@ -8,7 +8,7 @@ namespace SelfPractice_CodingProgramCSharp
 {
     public class FindLargerNum
     {
-        public static void Main(string[] args)
+        public  void GreaterNum()
         {
             Console.WriteLine("Enter Num1");
             int num1 = int.Parse(Console.ReadLine());
@@ -16,27 +16,26 @@ namespace SelfPractice_CodingProgramCSharp
             int num2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Num3");
             int num3 = int.Parse(Console.ReadLine());
-            //if (num1 > num2)
-            //{
+            // if (num1 > num2)
+            // {
             //    Console.WriteLine("Number1 is greater");
-            //}
-            //else
-            //{
+            // }
+            // else
+            // {
             //    Console.WriteLine("Number2 is greater");
-            //}
-            //if (num1 > num2 && num1 > num3)
-            //{
-            //    Console.WriteLine("Number1 is greater");
-            //}
-            //else if (num2 > num3 && num2 > num1)
-            //{
+            // }
+            // if (num1 > num2 && num1 > num3)
+            // {
+            // }
+            // else if (num2 > num3 && num2 > num1)
+            // {
             //    Console.WriteLine("Number2 is greater");
-            //}
-            //else
-            //{
+            // }
+            // else
+            // {
             //    Console.WriteLine("Number3 is greater");
-            //}
-            //Console.WriteLine(".............................");
+            // }
+            // Console.WriteLine(".............................");
 
             if (num1 > num2)
             {
