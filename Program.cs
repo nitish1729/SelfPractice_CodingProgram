8a90149d1f8c75e6af1826b60e9193709595350e
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-17 02:09:16 +0530

    practice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SelfPractice_CodingProgramCSharp;
+using selfpractice_codingprogramcsharp;
+
+// Console.WriteLine("Check given number is ood or even");
+// One_EveonOrOdd find = new One_EveonOrOdd();
+// find.check();
+
+// Console.WriteLine("Find Even Number between 1 to 100");
+// PrintAllEvenNum find1 = new PrintAllEvenNum();
+// find1.CheckEvenNum();
+
+  // Console.WriteLine("Find Odd Number between 1 to 100");
+  // OddNumber find2 = new OddNumber();
+  // find2.CheckOddNum();
+
+
+Console.WriteLine("Find Odd Number between 1 to 100");
+FindLargerNum find3 = new FindLargerNum();
+find3.GreaterNum();
